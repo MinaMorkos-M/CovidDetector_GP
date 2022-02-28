a1c69134d57605379d8c067db8d46180fc939195
MinaMorkos-M <63639438+MinaMorkos-M@users.noreply.github.com>
2022-02-28 19:13:23 +0200

    Documentation and Presentation are added

--- covid_19_detector/lib/helpers/constants.dart
+++ covid_19_detector/lib/helpers/constants.dart
@@ -2,4 +2,4 @@ const APP_VERSION = 'v1.0.0';
 const APP_NAME = 'Covid Detector';
 const APP_DESCRIPTION = 'An application showing the danger of corona virus in each area.'
     '\n\nDeveloped by Mina Morkos, Omar Shokry, Mayar El-hussieny, Esraa Ahmed and Abdelrahman Mohamed ';
-const GITHUB_URL = 'https://google.com/';
+const GITHUB_URL = 'https://github.com/MinaMorkos-M/CovidDetector_GP/tree/main/covid_19_detector';
